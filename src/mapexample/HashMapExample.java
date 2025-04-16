da63bb901a8dcbaeb3a600a82d7aa05a7245ddea
Chandan K M <chandanchintu16@gmail.com>
2025-04-16 20:49:59 +0530

    map examples

---
+++ src/mapexample/HashMapExample.java
@@ -0,0 +1,35 @@
+package mapexample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+
+        // declaring map
+        Map<Integer, String> map1 = new HashMap<Integer, String>();
+
+        // add data
+        map1.put(102, "Mango");
+        map1.put(104,"Guava");
+        map1.put(103,"Orange");
+        map1.put(105,"Apple");
+        map1.put(null,"Grapes");
+        map1.put(null,null);// last one gets overrided
+        map1.put(108,"Grapes");
+        map1.put(106,"Guava");
+        map1.put(103,"Pineapple");// last one get overrided
+        map1.put(107,"Orange");
+
+        System.out.println("map1 is : "+map1);
+
+        map1.remove(108);
+        System.out.println("map1 after removing 108 key is : "+map1);
+
+        // traversing
+        System.out.println("map traversing");
+        for(Map.Entry m1 : map1.entrySet()){
+            System.out.println(m1.getKey()+"--------"+m1.getValue());
+        }
+    }
+}
